7e924f8140c594d2c4a6f46147207e0c9e96e0c1
NahuelDiganchi <nahudiganchi18@gmail.com>
2024-06-10 11:38:48 -0300

    Arreglé un error en el create_app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -34,11 +34,6 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
  
-    return app  
-def create_app():
-    app = ...
-    # existing code omitted
-
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
